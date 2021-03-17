3debae19010369759df6dce7d651ad5bffa94635
chasscepts <francischassobetta@gmail.com>
2021-03-17 09:47:46 +0100

    implement single_pass method

--- sort.rb
+++ sort.rb
@@ -1,5 +1,14 @@
-def swap(array, idx1, idx2)
-  temp = array[idx1]
-  array[idx1] = array[idx2]
-  array[idx2] = temp
+# iterates over an array up to end index and swaps item positions
+# if current item is greater than next item
+
+def single_pass(array, end_index)
+  swaped = false
+  (0..end_index).each do |current_index|
+    next_index = current_index + 1
+    if array[current_index] > array[next_index]
+      array[current_index], array[next_index] = array[next_index], array[current_index]
+      swaped = true
+    end
+  end
+  swaped
 end
