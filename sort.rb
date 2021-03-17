f6a775b50a1fbe26488d8bd0504f5c67f3d9bf62
Addis Belete <addis.bel@gmail.com>
2021-03-17 12:23:16 +0300

    implement bubble sort

--- sort.rb
+++ sort.rb
@@ -3,7 +3,7 @@
 
 def single_pass(array, end_index)
   swaped = false
-  (0..end_index).each do |current_index|
+  (0...end_index).each do |current_index|
     next_index = current_index + 1
     if array[current_index] > array[next_index]
       array[current_index], array[next_index] = array[next_index], array[current_index]
@@ -12,3 +12,11 @@ def single_pass(array, end_index)
   end
   swaped
 end
+
+def bubble_sort(array)
+  result = array.clone { true }
+  end_index = result.length - 1
+  end_index -= 1 while single_pass(result, end_index)
+  result
+end
+print bubble_sort([3,2,5,7,6])
