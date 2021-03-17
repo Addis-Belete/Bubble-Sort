eef8a6431637a28898b7834a71547a2642dbae4d
Addis Belete <addis.bel@gmail.com>
2021-03-17 12:34:19 +0300

    implement single passing with block

--- sort.rb
+++ sort.rb
@@ -19,4 +19,15 @@ def bubble_sort(array)
   end_index -= 1 while single_pass(result, end_index)
   result
 end
-print bubble_sort([3,2,5,7,6])
+
+def single_pass_with_block(array, end_index)
+  swaped = false
+  (0...end_index).each do |current_index|
+    next_index = current_index + 1
+    if yield(array[current_index], array[next_index]).positive?
+      array[current_index], array[next_index] = array[next_index], array[current_index]
+      swaped = true
+    end
+  end
+  swaped
+end
